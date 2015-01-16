318dc631cdc7a8a2d013591e8bb071ca39d0d8f0
Ryan Sadio <ryan@sadio.ca>
2015-01-15 18:52:21 -0800

    Generate Gallery.

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -56,7 +56,29 @@ class Gallery extends Application {
      */
     public function index()
     {
-        //$this->load->view('gallery');
+        // Get all images from out model
+        $pix = $this->images->all();
+
+        // Build an array of formatted cells for them
+        foreach ($pix as $picture)
+        {
+            $cells[] = $this->parser->parse('_cell', (array) $picture, true);
+        }
+
+        // Prime the table class
+        $this->load->library('table');
+        $parms= array(
+            'table_open' => '<table class="gallery">',
+            'cell_start' => '<td class="oneimage">',
+            'cell_alt_start' => '<td class="oneimage">'
+        );
+        $this->table->set_template($parms);
+
+        // Finally generate the tables!
+        $rows = $this->table->make_columns($cells, 3);
+        $this->data['thetable'] = $this->table->generate($rows);
+
+        // Render page
         $this->data['pagebody'] = 'gallery';
         $this->render();
     }
