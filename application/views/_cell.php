75590e8af7f657b6ba4daffc48222352ee75c5d4
Ryan Sadio <ryan@sadio.ca>
2015-01-15 18:42:47 -0800

    Add view fragment for frontpage images

---
+++ application/views/_cell.php
@@ -0,0 +1,5 @@
+<a href="../../public/data/{filename}"
+   data-lightbox="gallery"
+   data-title="{title} ... Posted {uploaded} by {uploader}, in {category}">
+    <img src="../../public/data/thumb/{filename}"/>
+</a>
